3952e0530e682d109fc612a2d1b911b53607f68a
MOTOPROTOCOL <mamllechat11@gmail.com>
2025-02-25 13:08:38 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,38 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  // Preset for using TypeScript
   preset: 'ts-jest',
+  
+  // Run tests in a Node environment
   testEnvironment: 'node',
-  roots: ['<rootDir>/docs'],
+  
+  // Test file configuration
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+    '**/__tests__/**/*.test.ts',
+    '**/examples/**/*.test.ts'
   ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  
+  // Test setup file
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  
+  // Module file extensions
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  
+  // Code coverage settings
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
+  
+  // Timeout setting (extended for Solana transactions)
+  testTimeout: 30000,
+  
+  // Transform settings
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
   },
-  verbose: true,
-  testTimeout: 10000,
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  
+  // Mocking specific modules
+  moduleNameMapper: {
+    '@solana/web3.js': '<rootDir>/node_modules/@solana/web3.js',
+    '@metaplex-foundation/js': '<rootDir>/node_modules/@metaplex-foundation/js'
+  }
 };
