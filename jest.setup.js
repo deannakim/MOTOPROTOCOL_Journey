c1bffa365be2f8699e4ba85b446e00428629d12a
MOTOPROTOCOL <mamllechat11@gmail.com>
2025-03-06 10:43:32 +0900

    Delete jest.setup.js

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-// Jest setup file
-jest.setTimeout(30000); // Set test timeout
-
-// Add global settings if needed
-process.env.SOLANA_NETWORK = 'devnet';
-process.env.TOKEN_MINT = 'GccSrdDCs28Up6W8BdqDUwpSbJUAg2LXPRKPeQsNx6h';
